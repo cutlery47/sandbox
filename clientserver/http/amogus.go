6c76000c99d3bce8ce57caa1afe8cb14545ce037
Arkhip McNutt <ortiz47@mail.ru>
2024-02-19 22:06:43 +0300

    it's so over...

---
+++ clientserver/http/amogus.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("TU DU DU DU DU DU DU")
+    fmt.Println("TU DU DU")
+    fmt.Println("Sussy bakka was here!")
+    fmt.Println("uWu cum senpai aaaahhhh ^_^")
+    for i := range 10 {
+    }
+}
